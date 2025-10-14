e3f9b7f2d26fd05837e54a5dcb60101fe96db122
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-14 11:01:35 -0400

    chore: built api routes using express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,13 @@
 import express from 'express'
-
+import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
 //import cors from 'cors'
-//import bodyParser from 'body-parser'
-//import { postsRoutes } from './routes/posts.js'
 
 // Instatiate the application =================================================
 const app = express()
 //app.use(cors())
-//app.use(bodyParser.json())
-
-//postsRoutes(app)
+app.use(bodyParser.json())
+postsRoutes(app)
 
 // The root or default route ==================================================
 app.get('/', (req, res) => {
