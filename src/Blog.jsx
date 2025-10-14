853d9eff69dc6ab20b09c67633d0d3595bc90500
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-14 17:48:03 -0400

    chore: setup getting the posts

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,16 +1,31 @@
-//import { Post } from './components/Post.jsx'
+// import { Post } from './components/Post.jsx'
 import { PostList } from './components/PostList.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
 
+import { getPosts } from './api/posts.js'
+import { useQuery } from '@tanstack/react-query'
+
+/*
 const posts = [
   { title: 'full', contents: 'somestuff', author: 'me' },
   { title: 'sometitle else   weeee' },
 ]
+*/
 
 // Create main page ===========================================================
 export function Blog() {
+  // Create a useQuery instance ==============
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+
+  // Extract the data to be used ==============================================
+  const posts = postsQuery.data ?? []
+
+  // Form to display the application components ================================
   return (
     <div style={{ padding: 10 }}>
       <CreatePost />
---
+++ src/api/posts.js
@@ -0,0 +1,8 @@
+// API function to get the posts from the server ==============================
+export const getPosts = async (queryParams) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
+      new URLSearchParams(queryParams),
+  )
+  return await res.json()
+}
