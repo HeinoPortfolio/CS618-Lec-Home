443877209e66325032e222aa9d88031f70f4d967
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-16 02:52:31 -0400

    chore: integrated server side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './pages/Blog.jsx'
-import { Signup } from './pages/Signup.jsx'
-
-import { Login } from './pages/Login.jsx'
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import PropTypes from 'prop-types'
 
-// Client to call the backenend services ======================================
+// Client to call the backend services ========================================
 const queryClient = new QueryClient()
 
-//  Create the browser routes =================================================
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Blog />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
-
-// Create the application =====================================================
-export function App() {
+export function App({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </QueryClientProvider>
   )
+}
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
 }
