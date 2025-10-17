041870e60af52863c0c121a2345b55b185144fca
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-16 20:44:47 -0400

    feat:  implemented social media embeds

--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -4,8 +4,9 @@ import { useQuery } from '@tanstack/react-query'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
-
 import { Helmet } from 'react-helmet-async'
+
+import { getUserInfo } from '../api/users.js'
 
 // TRuncations function to keep tags to a maximum of 160 characters ===========
 function truncate(str, max = 160) {
@@ -26,14 +27,30 @@ export function ViewPost({ postId }) {
 
   const post = postQuery.data
 
+  // Get user information query ===============================================
+  const userInfoQuery = useQuery({
+    queryKey: ['users', post?.author],
+    queryFn: () => getUserInfo(post?.author),
+    enabled: Boolean(post?.author),
+  })
+
+  const userInfo = userInfoQuery.data ?? {}
+
   // Page for viewing the single post ========================================
   return (
     <div style={{ padding: 10 }}>
       {post && (
         <Helmet>
           <title>{post.title} | Full-Stack React Blog </title>
-          <title>{post.title} | Full-Stack React Blog</title>
           <meta name='description' content={truncate(post.contents)} />
+          <meta property='og:type' content='article' />
+          <meta property='og:title' content={post.title} />
+          <meta property='og:article:published_time' content={post.createdAt} />
+          <meta property='og:article:modified_time' content={post.updatedAt} />
+          <meta property='og:article:author' content={userInfo.username} />
+          {(post.tags ?? []).map((tag) => (
+            <meta key={tag} property='og:article:tag' content={tag} />
+          ))}
         </Helmet>
       )}
       <Header />
