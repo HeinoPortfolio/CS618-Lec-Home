3a83865883e06f722250f11baa50b24275f0a82d
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-21 16:48:00 -0400

    chore: setup basic socketio structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+// New ipmorts for socket.io module =========
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
 // Instatiate the application =================================================
 const app = express()
 app.use(cors())
@@ -13,9 +17,29 @@ app.use(bodyParser.json())
 postsRoutes(app)
 userRoutes(app)
 
+// The Socket IO code begins here =============================================
+// Create a socket.io server ==================================================
+const server = createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+// Setup a connection event  ==================================================
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id)
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id)
+  })
+})
+// End socket addition ====
+
 // The root or default route ==================================================
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
 })
 
-export { app }
+// export { app }
+export { server as app }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
-
 import { Login } from './pages/Login.jsx'
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
+
+// Imports for the socket.io module ===========================================
+import { io } from 'socket.io-client'
+
+// Create a socket ============================================================
+const socket = io(import.meta.env.VITE_SOCKET_HOST)
 
 // Client to call the backenend services ======================================
 const queryClient = new QueryClient()
@@ -25,6 +29,15 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ])
+
+// Event handlers =============================================================
+
+socket.on('connect', () => {
+  console.log('connected to socket.io as', socket.id)
+})
+socket.on('connect_error', (err) => {
+  console.error('socket.io connect error:', err)
+})
 
 // Create the application =====================================================
 export function App() {
